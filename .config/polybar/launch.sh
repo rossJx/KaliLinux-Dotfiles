0669c87385e6e4a36b7ffd0d2dc44cc5fb8ec093
Jovany Rossano <67485441+rossJx@users.noreply.github.com>
2020-12-13 13:58:38 -0600

    Add files via upload

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,6 +8,13 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
+# Launch bar1 at top and bar2 at bottom
 polybar top -c ~/.config/polybar/config-top.ini &
 #polybar bottom -c ~/.config/polybar/config-bottom.ini &
+
+#Launch polybar on external monitor
+external_monitor=$(xrandr -q | grep -w HDMI1| awk '{print $2}')
+if [ $external_monitor = 'connected' ]; then
+	polybar top_external -c ~/.config/polybar/config-top.ini &
+fi
+
